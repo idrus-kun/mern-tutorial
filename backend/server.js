6c041d7fb8a624807cc555f76b9e802db3c7b05b
Idruskun <191240000895@unisnu.ac.id>
2022-07-24 03:45:34 +0700

    add middleware

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
 
 const port = process.env.PORT || 5000
 
@@ -7,5 +8,7 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false}))
+
+app.use(errorHandler)
 
 app.use('/api/goals',require('./routes/goalRoutes.js'))
