b4dbbf3fac2396ce7d8fc014dd1cdd08a7daad2b
Idruskun <191240000895@unisnu.ac.id>
2022-07-24 03:22:27 +0700

    modified goalController

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,6 +1,37 @@
-const getGoals = (req,res) => {
-    res.status(200).json({message: 'Get Goals'})
-   }
-   module.exports = {
-    getGoals
-   }
+// @desc    Get goals
+// @route   GET /api/goals
+// @acccess Private
+
+    res.status(200).json({message: "Get Goals"})
+}
+
+// @desc    Set goals
+// @route   POST /api/goals
+// @acccess Private
+
+const setGoals = (req,res) => {
+    res.status(200).json({message: 'Set Goals'})
+}
+
+// @desc    Update goals
+// @route   PUT /api/goals
+// @acccess Private
+
+const updateGoals = (req,res) => {
+    res.status(200).json({message: 'Update Goal ${req.params.id}'})
+}
+
+// @desc    Delete goals
+// @route   DELETE /api/goals
+// @acccess Private
+
+const deleteGoals = (req,res) => {
+    res.status(200).json({message: 'Delete Goal ${req.params.id}'})
+}
+
+module.exports = {
+    getGoals,
+    setGoal,
+    updateGoal,
+    deleteGoal
+}
